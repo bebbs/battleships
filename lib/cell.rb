448b1c24489af282dae2c6d9e1b2c3ceb5e4a0f2
Andy <andygnewman@gmail.com>
2014-12-16 12:16:29 +0000

    add test for cell initialisation of content and hit

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,2 +1,16 @@
 class Cell
+
+  def initialize 
+    @content = :water
+    @hit = false
+  end
+
+  def content
+    @content
+  end
+
+  def hit?
+    @hit
+  end
+
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,5 +1,19 @@
 require 'cell'
 
 describe Cell do
+
+let(:cell) {Cell.new}
+
+  context 'a cell when initialised should' do
+    
+    it 'contain water' do
+      expect(cell.content).to eq(:water)
+    end
+
+    it 'not be hit' do
+      expect(cell.hit?).to eq(false)
+    end
+
+  end
   
 end
