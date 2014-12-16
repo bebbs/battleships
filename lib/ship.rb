6159b75bb42d8d11de9c94da872b8141b8c7b10a
Andy <andygnewman@gmail.com>
2014-12-16 09:53:34 +0000

    add ship initialisation tests

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,6 +1,8 @@
 class Ship
   def initialize(type)
     @size = ship_size(type)
+    @type = type
+    @hits_received = 0
   end
 
   def ship_size(type)
@@ -10,12 +12,20 @@ class Ship
       when :submarine then 3
       when :destroyer then 3
       when :patrol_boat then 2
-      else raise "Ship type not recognized!"
+      else raise 'Ship type not recognised!'
     end
   end
 
   def size?
     @size
+  end
+
+  def type?
+    @type
+  end
+
+  def hits_received?
+    @hits_received
   end
 
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -3,13 +3,31 @@ require 'ship'
 describe Ship do
 
   let(:ship) { Ship.new(:battleship) }
-  before(:each) do
-    allow(ship).to receive(:type)
-  end
+  # before(:each) do
+  #   allow(ship).to receive(:type)
+  # end
 
   context 'can be created with a' do
     it 'size' do
       expect(ship.size?).to eq(4)
     end
+
+    it 'type' do
+      expect(ship.type?).to eq(:battleship)
+    end
+
+    it 'hits received' do
+      expect(ship.hits_received?).to eq(0)
+    end
+
   end
+
+  context 'returns error if' do
+    
+    it 'not recognised ship parameter is passed' do
+      expect(lambda{Ship.new(:banana)}).to raise_error(RuntimeError, 'Ship type not recognised!')
+    end
+
+  end
+
 end
