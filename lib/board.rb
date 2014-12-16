f2177f92b11594d5cb63330b95aec630adef406a
Josh Bebbington <josh@focalwebsites.com>
2014-12-16 18:39:59 +0000

    Refactored board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,7 @@
 class Board
 
+  attr_reader :grid, :footprint_content, :footprint_array
+
   def initialize
     @grid = {}
     @columns = [*"A".."J"]
@@ -11,36 +13,42 @@ class Board
     end
   end
 
-  def grid
-    @grid
-  end
-
   def place_ship_unit(grid_ref)
     grid_ref.ship_in_cell!
   end
 
   def footprint(size, orientation, start_cell)
-    array = []
-    if orientation == :h
-      @columns[@columns.index(start_cell.to_s.slice(0)), size].each do |column_ref|
-        array << (column_ref + start_cell.to_s.slice(1)).to_sym
-      end
-    elsif orientation == :v
-      @rows[@rows.index(start_cell.to_s.slice(1).to_i), size].each do |row_ref|
-        array << (start_cell.to_s.slice(0) + row_ref.to_s).to_sym
-      end
-    else raise "Unrecognised orientation"
+    orientation == :h ? footprint_horizontal(size, start_cell) : footprint_vertical(size, start_cell)
+    outside_grid(size)
+  end
+
+  def footprint_horizontal(size, start_cell)
+    @footprint_array = []
+    @columns[@columns.index(start_cell.to_s.slice(0)), size].each do |column_ref|
+      @footprint_array << (column_ref + start_cell.to_s.slice(1)).to_sym
     end
-    raise 'Cannot place, ship goes outside grid' if array.length != size
-    array
+  end
+
+  def footprint_vertical(size, start_cell)
+    @footprint_array = []
+    @rows[@rows.index(start_cell.to_s.slice(1).to_i), size].each do |row_ref|
+      @footprint_array << (start_cell.to_s.slice(0) + row_ref.to_s).to_sym
+    end
+  end
+
+  def outside_grid(size)
+    raise 'Cannot place, ship goes outside grid' if @footprint_array.length != size
   end
 
   def check_footprint_content(footprint)
-    array = []
+    @footprint_content = []
     footprint.each do |grid_ref|
-      array << grid[grid_ref].content
+      @footprint_content << grid[grid_ref].content
     end
-    array
+  end
+
+  def footprint_unoccupied(array)
+    array.any? {|content| content == :water }
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -21,15 +21,22 @@ describe Board do
     end
 
     it 'should know which cells to place a ship in' do
-      expect(board.footprint(3, :v, :A1)).to eq([:A1, :A2, :A3])
+      board.footprint(3, :v, :A1)
+      expect(board.footprint_array).to eq([:A1, :A2, :A3])
     end
 
     it 'should return an error if footprint goes outside the grid' do
-      expect(lambda{board.footprint(4, :h, :I6)}).to raise_error(RuntimeError, 'Cannot place, ship goes outside grid')
+
+      expect{board.footprint(4, :h, :I6)}.to raise_error(RuntimeError, 'Cannot place, ship goes outside grid')
     end
 
     it 'should know the content of all cells in the footprint' do
-      expect(board.check_footprint_content([:A1, :A2, :A3])).to eq([:water, :water, :water])
+      board.check_footprint_content([:A1, :A2, :A3])
+      expect(board.footprint_content).to eq([:water, :water, :water])
+    end
+
+    it 'should return true if the footprint is clear' do
+      expect(board.footprint_unoccupied([:water, :water, :water])).to be true
     end
 
   end
