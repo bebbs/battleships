b06b74b63bac06c635c6cdd0fa5c177fc7a71e19
Andy <andygnewman@gmail.com>
2014-12-16 10:26:46 +0000

    add fleet initialisation test

--- lib/fleet.rb
+++ lib/fleet.rb
@@ -1,2 +1,21 @@
 class Fleet
+
+  def initialize  
+    @dock = []
+    dock_ship(Ship.new(:aircraft_carrier))
+    dock_ship(Ship.new(:battleship))
+    2.times {dock_ship(Ship.new(:destroyer))}
+    dock_ship(Ship.new(:submarine))
+    4.times {dock_ship(Ship.new(:patrol_boat))}
+  end
+
+
+  def dock
+    @dock
+  end
+
+  def dock_ship(ship)
+    dock << ship
+  end
+
 end
--- spec/fleet_spec.rb
+++ spec/fleet_spec.rb
@@ -2,4 +2,15 @@ require 'fleet'
 
 describe Fleet do
   
+  let(:fleet) {Fleet.new}
+
+  context 'fleet creation should' do 
+
+    it 'have 9 ships in total' do
+      expect(fleet.dock.length).to eq(9)
+    end
+
+    
+  end
+
 end
