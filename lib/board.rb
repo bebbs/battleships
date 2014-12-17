8eb2e08e8a4df7fce116dc49eb9de5b35de6b7ee
Andy <andygnewman@gmail.com>
2014-12-17 19:06:23 +0000

    refactor place ship wrapper method

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,30 @@
 class Board
 
-  attr_reader :grid, :footprint_content, :footprint_array
+  attr_reader :grid
 
   def initialize
     @grid = {}
-    @columns = [*"A".."J"]
-    @rows = [*1..10]
-    @columns.each do |letter|
-      @rows.each do |number|
-        @grid["#{letter}#{number}".to_sym] = Cell.new
-      end
+    columns = [*"A".."J"]
+    rows = [*1..10]
+    columns.each {|l| rows.each {|n| @grid["#{l}#{n}".to_sym] = Cell.new} }
     end
-  end
 
   def place_ship(ship, start_cell, orientation)
     coords = [start_cell]
-    (ship.size -1).times {coords << next_cell(coords, orientation)}
+    ((ship.size) -1).times {coords << next_cell(coords, orientation)}
+    put_on_grid_if_possible(coords, ship)
+    within_grid(coords)
+    check_coords_for_ships(coords)
+    place_ship_in_all_cells(coords, ship)
+  end
+
+  # def coordinates(ship, start_cell, orientation)
+  #   coords = [start_cell]
+  #   ((ship.size) -1).times {coords << next_cell(coords, orientation)}
+  #   return coords
+  # end
+
+  def put_on_grid_if_possible(coords, ship)
     within_grid(coords)
     check_coords_for_ships(coords)
     place_ship_in_all_cells(coords, ship)
@@ -45,61 +54,4 @@ class Board
     grid[grid_ref].ship_in_cell!(ship)
   end
 
-
-
-
-
-
-
-
-
-  def footprint(size, orientation, start_cell)
-    coords = [start_cell]
-    orientation == :h ? footprint_horizontal(size, start_cell) : footprint_vertical(size, start_cell)
-    outside_grid(size)
-  end
-
-    def footprint_horizontal(size, start_cell)
-      @footprint_array = []
-      @columns[@columns.index(start_cell.to_s.slice(0)), size].each do |column_ref|
-        @footprint_array << (column_ref + start_cell.to_s.slice(1)).to_sym
-      end
-    end
-
-    def footprint_vertical(size, start_cell)
-      @footprint_array = []
-      @rows[@rows.index(start_cell.to_s.slice(1).to_i), size].each do |row_ref|
-        @footprint_array << (start_cell.to_s.slice(0) + row_ref.to_s).to_sym
-      end
-    end
-
-  def check_footprint(size, footprint)
-    outside_grid(size)
-    get_footprint_content(footprint)
-  end
-
-    def outside_grid(size)
-      raise 'Cannot place, ship goes outside grid' if @footprint_array.length != size
-    end
-
-    def get_footprint_content(footprint)
-      @footprint_content = []
-      footprint.each do |grid_ref|
-        @footprint_content << grid[grid_ref].ship_or_water
-      end
-    end
-
-    def check_footprint_unoccupied(array)
-      array.all? {|content| content == :water }
-    end
-
-  def attempt_place_ship(ship, orientation, start_cell)
-    footprint(ship.size?, orientation, start_cell)
-    check_footprint(ship.size?, footprint_array)
-    place_ship(ship)
-  end
-
-
 end
-
-# place_ship wrapper method
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -15,11 +15,13 @@ describe Board do
   context 'refactoring place ships methods' do
 
     it 'should return the expected coordinates for a vertical ship of size 3' do
-      expect(board.coordinates(3, :A1, :vertical)).to eq([:A1, :A2, :A3])
+      ship_double = double :ship_double, size: 3
+      expect(board.coordinates(ship_double, :A1, :vertical)).to eq([:A1, :A2, :A3])
     end
 
     it 'should return the expected coordinates for a vertical ship of size 5' do
-      expect(board.coordinates(5, :C2, :horizontal)).to eq([:C2, :D2, :E2, :F2, :G2])
+      ship_double = double :ship_double, size: 5
+      expect(board.coordinates(ship_double, :C2, :horizontal)).to eq([:C2, :D2, :E2, :F2, :G2])
     end
 
     it 'should recognise if a ship is going outside the grid' do
@@ -38,16 +40,27 @@ describe Board do
 
     it 'should be able to place a ship in to a cell' do
       ship_double = double :ship_double
-      board.place_ship_in_cell(:A1, ship_double)
+      board.place_ship_in_a_cell(:A1, ship_double)
       expect(board.grid[:A1].ship_object).to eq(ship_double)
     end
 
     it 'should be able to place a ship in all its coordinates' do
       ship_double = double :ship_double
-      board.place_ship([:A1, :A2, :A3], ship_double)
+      board.place_ship_in_all_cells([:A1, :A2, :A3], ship_double)
       expect([:A1, :A2, :A3].all? {|grid_ref| board.grid[grid_ref].ship_object == ship_double}).to be true
     end
  
   end
+
+  context 'integration test for place_ship method' do
+
+    it 'should place a ship in the cells when passed ship, start cell and orientation' do
+      ship_double = double :ship_double, size: 4
+      board.place_ship(ship_double, :C4, :horizontal)
+      expect([:C4, :D4, :E4, :F4].all? {|grid_ref| board.grid[grid_ref].ship_object == ship_double}).to be true
+    end
+
+  end
+
 
 end
