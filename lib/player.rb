658c568735c224962fc7b9cc05260a94a74092db
Andy <andygnewman@gmail.com>
2014-12-18 09:29:13 +0000

    add tests for player to have fleet and name

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,23 @@
 class Player
 
-  attr_reader :board
+  attr_reader :name, :board, :fleet
 
-  def initialize(content)
-    board_setup(content)
+  def initialize(player_name, board_content, fleet_content)
+    player_name_setup(player_name)
+    board_setup(board_content)
+    fleet_setup(fleet_content)
   end
 
-  def board_setup(content)
-    @board = content.new(Cell)
+  def player_name_setup(player_name)
+    @name = player_name
+  end
+
+  def board_setup(board_content)
+    @board = board_content.new(Cell)
+  end
+
+  def fleet_setup(fleet_content)
+    @fleet = fleet_content.new
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,15 +2,26 @@ require 'player'
 
 describe Player do
 
-  let(:player){Player.new(board_double)}
+  let(:player){Player.new(player1, board_double, fleet_double)}
+  let(:player1){double :player1}
   let(:board_double){double :board_double, new: board}
+  let(:fleet_double){double :fleet_double, new: fleet}
   let(:board){double :board}
+  let(:fleet){double :fleet}
+
 
   context 'a player can' do
 
     it 'have a board' do
-      allow(player).to receive(:board_setup).with(Board)
       expect(player.board).to eq(board)
+    end
+
+    it 'have a fleet' do
+      expect(player.fleet).to eq(fleet)
+    end
+
+    it 'have a name' do
+      expect(player.name).to eq(player1)
     end
 
   end
